45777e78e5aa8f5b393c7ac6fb2409a503e49d75
ahmed mustafa <am7413780@gmail.com>
2024-07-04 14:07:39 +0300

    new changes

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-def main():
-    # Create the model
-    model = create_model()
+import pandas as pd
+import numpy as np
 
-    # Train the mode
-    train(model)
+def get_clean_data():
+    data = pd.read_csv('./data/data.csv')
+    data = data.drop(['Unnamed: 32', 'id'], axis=1)
+    data['diagnosis'] = data['diagnosis'].map({'M': 1, "B": 0})
+
+    print(data.head())
+    return data
+
+def main():
+    data = get_clean_data()
+
+    # Create the model
+    # model = create_model()
+
+    # Train the model
+    # train(model)
 
     # Evaluate the model
-    evaluate(model)
+    # evaluate(model)
 if __name__== '__main__':
     main()
